8d8b4472bc773dd33b55288a69bfed8e985e03cc
lip <misterlizhaopeng@163.com>
2020-04-09 01:26:21 +0800

    List<String> list=Arrays.asList()

--- spring-ioc/src/main/java/cn/com/config/mt.java
+++ spring-ioc/src/main/java/cn/com/config/mt.java
@@ -3,17 +3,19 @@ package cn.com.config;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class mt {
 	@Test
 	public void componentTest() {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(HouseConfig.class);
 		String[] names = ctx.getBeanDefinitionNames();
-		System.out.println(1);
-		for (String ss : names) {
-			System.out.println(ss);
-		}
-		
-		
+		List<String> list = Arrays.asList(names);
+		list.forEach(a->{
+			System.out.println(a);
+		});
+
 //		House bean = ctx.getBean("house01",House.class);
 //		System.out.println("---------->"+bean);
 	}
